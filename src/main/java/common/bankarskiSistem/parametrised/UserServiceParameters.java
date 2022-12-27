73bd7d12848e31c0ea0a2df83d58b2b33d7303b4
Mirzad <mirzadtahirovic00@outlook.com>
2022-12-27 13:28:27 +0100

    Finished task

--- src/main/java/common/bankarskiSistem/parametrised/UserServiceParameters.java
+++ src/main/java/common/bankarskiSistem/parametrised/UserServiceParameters.java
@@ -3,6 +3,7 @@ package common.bankarskiSistem.parametrised;
 import common.bankarskiSistem.model.*;
 import org.junit.jupiter.params.provider.Arguments;
 
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public final class UserServiceParameters {
@@ -36,6 +37,54 @@ public final class UserServiceParameters {
 
         BankAccount bankAccount = new BankAccount(accountType,currency,user,bank,account_id);
         user.addAccount(bankAccount);
+
+        return  Stream.of(
+                Arguments.of(user,bankAccount)
+        );
+    }
+
+    public static Stream<Arguments> generateBankAccountWithoutUser(){
+
+        Conversion conversion = new Conversion(
+                1,
+                Currency.EUR,
+                Currency.RSD,
+                117,
+                null
+        );
+
+        Conversion conversion2 = new Conversion(
+                2,
+                Currency.RSD,
+                Currency.EUR,
+                0.0085,
+                null);
+
+        ArrayList<Conversion> conversions = new ArrayList<>();
+        conversions.add(conversion);
+        conversions.add(conversion2);
+
+
+        String personalId = "2011445745511";
+        String user_name = "Pera";
+        String user_surname = "Petrovic";
+        String user_address = "Adr 1";
+
+        User user = new User(personalId, user_name, user_surname, user_address);
+
+        Integer id_bank = 1;
+        String bank_address = "Mihaila Pupina 13";
+        String bank_name = "Banka Intesa";
+
+        Bank bank = new Bank(id_bank, bank_name, bank_address);
+
+        ExchangeRates exchangeRates = new ExchangeRates(1,null,conversions,null);
+
+        Integer account_id = 1;
+        AccountType accountType = AccountType.FOREIGN;
+        Currency currency = Currency.EUR;
+
+        BankAccount bankAccount = new BankAccount(accountType,currency,user,bank,account_id);
 
         return  Stream.of(
                 Arguments.of(user,bankAccount)
--- src/test/java/common/bankarskiSistem/unit/service/UserServiceUnitTests.java
+++ src/test/java/common/bankarskiSistem/unit/service/UserServiceUnitTests.java
@@ -1,25 +1,23 @@
 package common.bankarskiSistem.unit.service;
 
+import common.bankarskiSistem.exceptions.EntityNotFoundException;
 import common.bankarskiSistem.model.*;
 import common.bankarskiSistem.repository.BankAccountRepository;
+import common.bankarskiSistem.repository.ConversionRepository;
 import common.bankarskiSistem.repository.UserRepository;
+import common.bankarskiSistem.service.ConversionService;
 import common.bankarskiSistem.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mapstruct.control.MappingControl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,6 +27,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class UserServiceUnitTests {
 	@InjectMocks
 	private UserService userService;
+	@InjectMocks
+	private ConversionService conversionService;
 
 	@Mock
 	private UserRepository userRepository;
@@ -36,9 +36,15 @@ class UserServiceUnitTests {
 	@Mock
 	private BankAccountRepository bankAccountRepository;
 
+	@Mock
+	private ConversionRepository conversionRepository;
+
+
+
+
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateUser")
-	public void whenValidPersonalId_thenUserShouldBeFound(User user) {
+	public void getUserByPersonalID_found(User user) {
 
 		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
 
@@ -49,7 +55,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateUser")
-	public void whenNonValidPersonalId_thenNullPointerExceptionIsThrown(User user) {
+	public void getUserByPersonalID_nullPersonalId_NullPointerExceptionIsThrown(User user) {
 
 		Exception exception = assertThrows(NullPointerException.class, () ->{
 			userService.getUserByPersonalID(null);
@@ -63,7 +69,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateUser")
-	public void whenNonExistingPersonalId_thenUserShouldNotBeFound(User user) {
+	public void getUserByPersonalID_nonExistingUser_userNotFound(User user) {
 
 		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.empty());
 
@@ -80,8 +86,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
-	public void whenValidBankAccountId_thenBankAccountShouldBeFound(User user, BankAccount bankAccount) {
-
+	public void getBankAccountByID_found(User user, BankAccount bankAccount) {
 
 		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
 
@@ -94,19 +99,34 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateUser")
-	public void whenNonValidBankAccountId_thenNullShouldBeFound(User user) {
+	public void getBankAccountByID_nonValidBankAccountId_nullReturned(User user) {
 		Integer account_id = 1;
 
 		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
 
 		BankAccount found = userService.getBankAccountByID(user.getPersonalId(),account_id);
-//verify samo :D
+
 		assertThat(found).isNull();
 	}
 
 	@ParameterizedTest
+	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
+	public void deleteBankAccountById_deleted(User user, BankAccount bankAccount) throws EntityNotFoundException {
+
+
+		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
+
+		BankAccount found = userService.getBankAccountByID(user.getPersonalId(),bankAccount.getIdAccount());
+
+		assertThat(userService.deleteBankAccountById(user.getPersonalId(),found.getIdAccount())).isNotNull();
+		assertThat(found).isNotNull();
+
+		Mockito.verify(userRepository, Mockito.times(1)).save(user);
+	}
+
+	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateUser")
-	public void whenNonValidBankAccountId_thenNullPointerExceptionIsThrownDeletingBankAccount(User user) {
+	public void deleteBankAccountById_nonValidAccountId_NullPointerExceptionIsThrown(User user) {
 
 		Exception exception = assertThrows(NullPointerException.class, () ->{
 			userService.deleteBankAccountById(user.getPersonalId(),null);
@@ -120,7 +140,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
-	public void whenNonValidPersonalID_thenNullPointerExceptionIsThrownDeletingBankAccount(User user, BankAccount bankAccount) {
+	public void deleteBankAccountById_nonValidPersonalId_NullPointerExceptionIsThrown(User user, BankAccount bankAccount) {
 
 		Exception exception = assertThrows(NullPointerException.class, () ->{
 			userService.deleteBankAccountById(null,bankAccount.getIdAccount());
@@ -132,13 +152,51 @@ class UserServiceUnitTests {
 		assertEquals(expectedMessage,actualMessage);
 	}
 
-	@Test
-	public void whenNonValidPersonalID_thenNullPointerExceptionIsThrownGettingBalance() {
+	@ParameterizedTest
+	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccountWithoutUser")
+	public void deleteBankAccountById_nonValidPersonalId_EntityExceptionIsThrown(User user, BankAccount bankAccount) {
+		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
+
+		Exception exception = assertThrows(EntityNotFoundException.class, () ->{
+			userService.deleteBankAccountById(user.getPersonalId(), bankAccount.getIdAccount());
+		});
+
+		String expectedMessage = "User " + user.getPersonalId() + " doesn't have account " + bankAccount.getIdAccount();
+		String actualMessage = exception.getMessage();
+
+		assertEquals(expectedMessage,actualMessage);
+	}
+
+/*	@ParameterizedTest
+	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
+	public void getBalance_okWithToCurrency(User user, BankAccount bankAccount) throws EntityNotFoundException {
+		Currency currency = Currency.RSD;
+		Conversion conversion = new Conversion(
+				1,
+				Currency.EUR,
+				Currency.RSD,
+				117,
+				null
+		);
+
+		Mockito.when(
+				conversionRepository.findByCurrencyFromAndCurrencyToAndExchangeRates(
+				bankAccount.getCurrency(), currency, bankAccount.getBank().getExchangeRates())
+		).thenReturn(Optional.of(conversion));
+		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
+
+		assertThat(conversionService.convert(bankAccount.getCurrency(),currency,bankAccount.getBank())).isNotNull();
+		assertThat(userService.getBalance(user.getPersonalId(), bankAccount.getIdAccount(),Optional.of(currency))).isNotNull();
+	}*/
+
+	@ParameterizedTest
+	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
+	public void getBalance_NonValidPersonalID_NullPointerException(User user, BankAccount bankAccount) {
 		Integer accountID = 1;
 		Currency currency = Currency.EUR;
 
 		Exception exception = assertThrows(NullPointerException.class, () ->{
-			userService.getBalance(null,accountID,Optional.of(currency));
+			userService.getBalance(null,bankAccount.getIdAccount(),Optional.of(currency));
 		});
 
 		String expectedMessage = "Null personal id";
@@ -149,7 +207,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
-	public void whenNonAccountID_thenNullPointerExceptionIsThrownGettingBalance(User user, BankAccount bankAccount) {
+	public void getBalance_nullAccountId_NullPointerException(User user, BankAccount bankAccount) {
 
 		Exception exception = assertThrows(NullPointerException.class, () ->{
 			userService.getBalance(user.getPersonalId(),null,Optional.of(bankAccount.getCurrency()));
@@ -163,7 +221,7 @@ class UserServiceUnitTests {
 
 	@ParameterizedTest
 	@MethodSource("common.bankarskiSistem.parametrised.UserServiceParameters#generateBankAccount")
-	public void whenNonValidCurrency_thenNullPointerExceptionIsThrownGettingBalance(User user, BankAccount bankAccount) {
+	public void getBalance_nullCurrency_NullPointerException(User user, BankAccount bankAccount) {
 
 		Mockito.when(userRepository.findByPersonalId(user.getPersonalId())).thenReturn(Optional.of(user));
 
